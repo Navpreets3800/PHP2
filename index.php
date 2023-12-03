a51666b8ab330be27e08f7599fe747466b858921
Navpreets3800 <136133389+Navpreets3800@users.noreply.github.com>
2023-12-02 23:29:21 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/styles.css">
+    <title>Registration and Login</title>
+</head>
+<body>
+
+    <?php include 'templates/header.php'; ?>
+
+    <div class="content-container">
+        <div class="form-container">
+            <h2>Welcome for the booking!</h2>
+            <p>Please choose an option:</p>
+            <ul>
+                <li><a href="signup.php">New account</a></li>
+                <li><a href="login.php">Exsisting account</a></li>
+            </ul>
+        </div>
+    </div>
+
+    <?php include 'templates/footer.php'; ?>
+
+</body>
+</html>
+
